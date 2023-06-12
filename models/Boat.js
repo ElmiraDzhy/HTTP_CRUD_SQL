de2c2b656d31b5fad065d89b21948ba42daa14d1
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-12 10:54:06 +0300

    Add Boat model with CRUD methods

--- models/Boat.js
+++ models/Boat.js
@@ -1,4 +1,57 @@
 class Boat {
-    constructor () {
+    static _client = null;
+    static _tableName = 'boats';
+    static _attributes = {
+        name: 'string',
+        is_sea_able: 'boolean',
+        created_at: 'string',
+        water_displacement: 'int',
+        max_speed: 'int'
+    }
+
+    static async create (insertValues) {
+        const insertAttr = Object.entries(this._attributes)
+            .filter(([attr, domain]) => attr in insertValues)
+            .map(([attr]) => attr);
+        const insertSchemaAttr = insertAttr.map(attr => `"${attr}"`).join(',');
+
+        const insertValuesStr = insertAttr.map(attr => {
+            const value = insertValues[attr];
+            return typeof value === 'string' ? `'${value}'` : value;
+        }).join(',');
+
+        const str = `INSERT INTO ${this._tableName} (${insertSchemaAttr})
+                     VALUES (${insertValuesStr})
+                     RETURNING *;`;
+        const {rows} = await this._client.query(str);
+        return rows;
+    }
+
+    static async findByPk (pk) {
+        const {rows} = await this._client.query(`SELECT *
+                                                 FROM ${this._tableName}
+                                                 WHERE ${this._tableName.id} = ${Number(pk)};`);
+        return rows;
+    }
+
+    static async findAll () {
+        const {rows} = await this._client.query(`SELECT *
+                                                 FROM ${this._tableName};`);
+
+        return rows;
+    }
+
+    static async deleteByPk (pk) {
+        const {rows: deleted_row} = await this._client.query(`DELETE
+                                                              FROM ${this._tableName}
+                                                              WHERE ${this._tableName.id} = ${Number(pk)}
+                                                              RETURNING *;`);
+        return deleted_row;
+    }
+
+    static async updateByPk () {
+
     }
 }
+
+module.exports = Boat;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,13 @@
-const pg = require('pg')
+const {Client} = require('pg');
+const configs = require('./../configs/db.json');
+const Boat = require('./Boat');
+const dbConfigs = configs['development'];
+const client = new Client(dbConfigs);
+
+client.connect();
+Boat._client = client;
+
+module.exports = {
+    Boat,
+    client
+}
